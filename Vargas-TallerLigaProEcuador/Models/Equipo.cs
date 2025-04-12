4126b097ef4cc39c527426870995d74ed8817ac9
Pablofvb07 <pablovargas44@gmail.com>
2025-04-12 18:59:05 -0500

    Agregar archivos de proyecto.

---
+++ Vargas-TallerLigaProEcuador/Models/Equipo.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vargas_TallerLigaProEcuador.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [DisplayName("Nombre de Equipo")]
+        [Required]
+        public string Nombre {  get; set; }
+        [Range(0,20)]
+        public int PartidosJugados {  get; set; }
+        [Range(0, 20)]
+        public int PartidosGanados {  get; set; }
+        [Range(0, 20)]
+        public int PartidosEmpatados {  get; set; }
+        [Range(0, 20)]
+        public int PartidosPerdidos {  get; set; }
+        [Range(0, 20)]
+        public int Puntos {
+            get {
+                int puntos = PartidosGanados * 3 + PartidosEmpatados * 1;
+            return 10; 
+            }
+    }
+
+    }
+}
---
+++ Vargas-TallerLigaProEcuador/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vargas_TallerLigaProEcuador.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
